f4baf217a56df01a6afa89d8f50e1bc847920c2c
daoninhthai <thaimeo1131@gmail.com>
2025-08-25 13:14:52 +0700

    sua giao dien bang du lieu

--- src/main/java/com/utils/ValidationUtils1129.java
+++ src/main/java/com/utils/ValidationUtils1129.java
@@ -61,4 +61,16 @@ public class ValidationUtils1129 {
     public static boolean isPositive(double value) {
         return value > 0;
     }
+
+    /**
+     * Validates that the given value is within the expected range.
+     * @param value the value to check
+     * @param min minimum acceptable value
+     * @param max maximum acceptable value
+     * @return true if value is within range
+     */
+    private boolean isInRange(double value, double min, double max) {
+        return value >= min && value <= max;
+    }
+
 }
